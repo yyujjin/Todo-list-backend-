6cb39b89ec1034e734c68c86420d13b8208b5c87
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-10-18 23:44:25 +0900

    delete route 연결

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
-
+//main 은 연결돼있어서 저기선 쓸필요없음. 
 func main() {
 	r := gin.Default()
 	
@@ -15,27 +15,12 @@ func main() {
 	// todos -> todos 슬라이스에 있는 값을 전달
 	// ['todo1', 'todo2', 'todo3']
 	
+
+	//라우터에있는 익명함수를 다른파일로 분리해주는거
+	//다른파일에 함수가 있으면 대문자로 해줘야 쓸수가이씀.
 	r.GET("/todos", router.Todos) 
-
+	r.POST("/todos", router.PostTodos) 
+	r.DELETE("/todos", router.DeleteTodos) 
 	
-	// post method/ todos -> todo4 요소 추가 후 todos slice response
-	// r.POST("/todos", func(c *gin.Context) {
-	// 	todos = append (todos,"todo4")
-	// 	c.JSON(200, gin.H{
-			
-	// 		"todos": todos, 
-
-	// 	})
-	// })
-
-	// // delete method / todos -> index 0 delete 
-	// r.DELETE("/todos", func(c *gin.Context) {
-	// 	todos =  todos[1:] 
-	// 	c.JSON(200, gin.H{
-			
-	// 		"todos": todos, 
-
-	// 	})
-	// })
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
--- router/router.go
+++ router/router.go
@@ -9,14 +9,34 @@ func Pong(c *gin.Context) {
 	})
 }
 
+var todos = []string { // TODO전역변수로 만들기
+	"todo1",
+	"todo2", 
+	"todo3",
+}
+
 func Todos(c *gin.Context) {
-	todos := []string { // TODO 전역변수로 만들기
-		"todo1",
-		"todo2", 
-		"todo3",
-	}
+	
 
 	c.JSON(200, gin.H{
 		"todos": todos, 
+	})
+}
+
+func PostTodos(c *gin.Context) {
+	todos = append (todos,"todo4")
+	c.JSON(200, gin.H{
+		
+		"todos": todos, 
+
+	})
+}
+
+func DeleteTodos(c *gin.Context) {
+	todos = todos[1:]
+	c.JSON(200, gin.H{
+		
+		"todos": todos, 
+
 	})
 }
