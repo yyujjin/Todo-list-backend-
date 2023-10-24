efc54c906bda672b3b3aff34927f9a3b5e2219e7
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-10-24 23:42:34 +0900

    post route 연결

--- router/router.go
+++ router/router.go
@@ -39,10 +39,14 @@ func Todos(c *gin.Context) {
 }
 
 func PostTodos(c *gin.Context) {
-	// todos = append (todos,"todo4") // 숙제
+	// 1. todo3을 슬라이스에 추가
+	todos = append(todos,todo{3,"todo3"})
+	// 2. todos 슬라이스를 출력(응답)
+	fmt.Println(todos)
+
 	c.JSON(200, gin.H{
 		
-		"todos": todos, 
+		"todos": todos, // todo1~todo3
 
 	})
 }
