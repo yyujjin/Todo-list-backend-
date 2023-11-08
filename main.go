9eb6fffe4c8a78fb6386135bb715ebf19c4f6119
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-11-08 22:40:54 +0900

    delete route 연결

--- main.go
+++ main.go
@@ -23,24 +23,18 @@ func main() {
 	// ['todo1', 'todo2', 'todo3']
 	
 
-	// 1. 클라이언트가 /todos를 호출하면 접근하면 함수가 실행됨.
-	// 2.  이 라우터는 배열을 내보내는 라우터.
-	// [{id: 1, name: 'todo1'}] => 원하는 형태
-	//필드 네임을 앞글자 대문자로 해야지 화면에 나타나는데
-	//보통은 소문자로 시작하기때문에 옆에 json을 넣어줘서 소문자로 만들어줘. 
+	
 	type todo struct {
 		Id int `json:"id"`
 		Name string `json:"name"`
 	}
 
-	//3.todos라는 배열을 만드는데 구조체를 자료형으로가진 배열
-	//4.todo구조체만 넣을수있는 구조라서 []todo{todo{1,"todo"}}=>이렇게 안해줘도 됨. 
+
 	todos := []todo{{1,"todo1"},{2,"todo2"}}
 
 	r.GET("/todos", func (c *gin.Context) {
 	
-	//JSON = 응답해주는 함수
-	//todos 배열을 내보내겠다는 의미 
+	 
 	fmt.Println(todos)
 		c.JSON(200, todos)
 	}) 
@@ -48,39 +42,58 @@ func main() {
 	//5. post : 요소 추가 {id: 3, name: 'todo3'} => 언니가 원하는 형태
 	
 	r.POST("/todos", func (c *gin.Context) {
-		//append로 넣어줄때는 자료형(구조체)을 기입해야 컴퓨터가 알아먹음
-		//POST로 요청하면 어펜드로 값을 넣어주고
-		//JOSN은 추가된 todos배열 자체를 보여준다는 뜻.
-		//newtodo라는 변수를 만들고 데이터타입은 todo구조체를 한다
+		//append로 넣어줄때는 자료형(구조체)을 기입해야 컴퓨터가 알아먹음  ???
+			
 		var newTodo todo
-
-		//클라이언트가 바디로 보낸값을 변수에다 넣어주는 코드 밑
+		//&이거 왜 쓰는지 모르겠고
+		//newTodo변수는 그럼 처음엔 그냥 선언됐다가
+		//바디에 담긴 값을 변수에 저장하면 계속 값이 바뀌는 건가?
 		if err := c.BindJSON(&newTodo); err != nil {
 			return
 		}
-		// 그리고 값이 담긴 그 변수를 todos 배열에 넣어줌.
+
 		todos = append(todos,newTodo)
-		//클라이언트한테 니가준거 잘들어갔다고 응답해줌
 		c.JSON(http.StatusCreated, newTodo )
 	}) 
-
+	 
+//:id를 클라이언트가 입력하면 string으로 값이 들어오는건가?????
 	r.GET("/todos/:id", func (c *gin.Context) {
 		//밑에 코드를 하는 이유는 ?'
 		//for range로 변수에 담긴 값고 비교해야 하는데
-		//todo구조체의 id필드는 string 이니까 
-		//클라이언트가 요청한 id값 int랑 자료형이 맞지않아 비교가 안돼서
-		//todo 필드 id값을 int로 바꿔주는 코드.
-		id,_ := strconv.Atoi (c.Param("id"))  
+		//todo구조체의 id필드는 string 이니까  ==>>int인데??
+		//클라이언트가 요청한 id값 int랑 자료형이 맞지않아 비교가 안돼서  ==>클라이언트가 요청한 값이 string아님??
+		//todo 필드 id값을 int로 바꿔주는 코드.  ==>>새로만든 id의 자료형이 string이니까 그걸 int로 바꿔주는 거 아님???
+		id,_ := strconv.Atoi (c.Param("id"))  //자료형이 string 이라서  "" 이 안에 쓴건가???
 		for _, a := range todos {
 			if a.Id == id {
 				c.IndentedJSON(http.StatusOK, a)
-				return
+				return  // 근데 이 return 한 값이 어디가는거임?
 			}
 		}
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 	})
 
+	//슬라이스 중간ㄴ 삭제 법
+	// slice = append(slice[:1], slice[2:]...)
+
+
+	r.DELETE("/todos/:id", func (c *gin.Context) {
+		id,_ := strconv.Atoi (c.Param("id"))  
+		for index, a := range todos {
+			if a.Id == id {
+				c.IndentedJSON(http.StatusOK, a)
+				todos = append(todos[:index],todos[index+1:]...)
+				// new :=todos[index:]
+				// fmt.Println(new)
+				return  
+			}
+		}
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	})
+
+
 	
+
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
